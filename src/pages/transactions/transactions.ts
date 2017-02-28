1626eff5cb72eaa552404fdf8919867348f9aece
asder707 <abn.sebick@gmail.com>
2017-02-28 17:10:58 -0300

    crear proyecto, primera vista, configurar base de datos, modelo de datos para movimientos

---
+++ src/pages/transactions/transactions.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+import {Transaction} from '../../database';
+
+/*
+  Generated class for the Transactions page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-transactions',
+  templateUrl: 'transactions.html'
+})
+export class TransactionsPage {
+
+  title : String = "Movimientos";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    //let transaction = new Transaction(20,"Primera Transaccion");
+    //transaction.save();
+  }
+
+}
