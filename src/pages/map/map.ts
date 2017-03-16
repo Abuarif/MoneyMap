d6b6570f23a0a1d3c9cb8740693ae309418ddd0f
asder707 <abn.sebick@gmail.com>
2017-03-16 17:44:24 -0300

    se definió instancia del mapa con parametros, solo para sdk de android

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
+import {GeolocationService} from '../../services/geolocation.service'
 
 /*
   Generated class for the Map page.
@@ -13,10 +15,40 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class MapPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public geolocator : GeolocationService) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
+    //obtener ubicacion del usuario para centrar mapa
+    this.geolocator.get().then((result)=>{
+      //cargar mapa
+      this.loadMap(result.coords.latitude, result.coords.longitude);
+    }).catch((err)=>console.log(err));
+  }
+
+  loadMap(lat,lng){
+    let location : GoogleMapsLatLng = new GoogleMapsLatLng(lat,lng);
+
+    new GoogleMap("map",{
+      'controls': {
+        'compass':true,
+        'myLocationButton':true,
+        'indoorPicker':true,
+        'zoom':true
+      },
+      'gestures':{
+        'scroll':true,
+        'tilt':true,
+        'rotate':true,
+        'zoom':true
+      },
+      'camera':{
+        'latLng': location,
+        'tilt':30,
+        'zoom':15,
+        'bearing':50
+      }
+    });
   }
 
 }
