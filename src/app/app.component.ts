fb0de787dbea2150e9eb45cdc4ec69a9ee5b33d3
asder707 <abn.sebick@gmail.com>
2017-03-20 17:57:00 -0300

    no funciona-> tratando de arreglar play serices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 //import db from '../database';
 
 import { TabsPage } from '../pages/tabs/tabs';
-
+//esto es para que no mande errores typescript, pero javascript no tiene problemas
+declare var FCMPlugin;
 
 @Component({
   templateUrl: 'app.html'
@@ -17,6 +18,14 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+
+      //device token ->coneccion entre dispotivos
+      FCMPlugin.getToken(function(token){
+        alert(token);
+      },function(err){
+        console.log("No se pudo obtener token"+err);
+      });
+
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
